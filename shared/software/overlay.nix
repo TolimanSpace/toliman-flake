e06d240ca5073a313f87a8ec12a46b57e413183a
Clan2098 <134997657+CLangford2098@users.noreply.github.com>
2024-03-04 15:21:21 +1100

    Sets nvidia-jetpack

--- shared/software/overlay.nix
+++ shared/software/overlay.nix
@@ -2,4 +2,5 @@ final: prev: {
   spinnaker = final.callPackage ./spinnaker { };
   spinnaker-acquisition = final.callPackage ./spinnaker-acquisition { };
   jax-test = final.callPackage ./jax-test { };
+  cudaPackagesGoogle = nvidia-jetpack.cudaPackages;
 }

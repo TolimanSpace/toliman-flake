6d4ee6e2608a979ea372ca0cf7f9319220894bc6
Clan2098 <134997657+CLangford2098@users.noreply.github.com>
2024-03-04 15:24:03 +1100

    Fixes import of nvidia-jetpack

--- shared/software/overlay.nix
+++ shared/software/overlay.nix
@@ -2,5 +2,5 @@ final: prev: {
   spinnaker = final.callPackage ./spinnaker { };
   spinnaker-acquisition = final.callPackage ./spinnaker-acquisition { };
   jax-test = final.callPackage ./jax-test { };
-  cudaPackagesGoogle = prev.nvidia-jetpack.cudaPackages;
+  cudaPackagesGoogle = final.nvidia-jetpack.cudaPackages;
 }

e27a211610584b6abf07d7e5a2367932fc10e86a
Clan2098 <134997657+CLangford2098@users.noreply.github.com>
2024-03-12 09:49:58 +1100

    Adds Nvidia settings

--- shared/default.nix
+++ shared/default.nix
@@ -3,11 +3,37 @@
 {
   imports = [ ./software ./state-version.nix ];
 
-  # Enable Anduril's Jetpack modules
-  hardware.nvidia-jetpack = {
-    enable = true;
-    som = "xavier-nx-emmc";
-    carrierBoard = "devkit"; # devkit enables fan control
+  # # Enable Anduril's Jetpack modules
+  # hardware.nvidia-jetpack = {
+  #   enable = true;
+  #   som = "xavier-nx-emmc";
+  #   carrierBoard = "devkit"; # devkit enables fan control
+  # };
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   # Shared bootloader config, specifies
@@ -28,5 +54,11 @@
 
   nixpkgs.config.allowUnfree = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl.enable = true;
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
 }

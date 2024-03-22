7dfc0a15be436d082fb57b7bc7f5ed6383f1d143
clan2098 <clan2098@clan2098s-air.staff.wireless.sydney.edu.au>
2024-03-22 14:05:53 +1100

    Updates nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # NixOS 23.11
     # Avoid changing it in production, but changing before production is ok
-    nixpkgs.url = "github:arduano/nixpkgs?rev=54a86737624ba3b1e63f5e6ff26aac3972509f36";
+    nixpkgs.url = "github:arduano/nixpkgs?rev=b06025f1533a1e07b6db3e75151caa155d1c7eb3";
 
     vscode-server = {
       url = "github:nix-community/nixos-vscode-server";
